7e096990318f9667083ab2914985e00635e4f332
Andreas Rozek <A.Rozek@gmx.de>
2024-06-11 16:01:53 +0200

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import dts  from 'vite-plugin-dts'
+import path from 'path'
+
+export default defineConfig({
+  build: {  	
+  	lib: {
+  		entry:   path.resolve(__dirname, 'src/shareableNoteStickers.ts'),
+  		name:    'shareable-note-stickers',
+  		formats: ['es']
+  	},
+  	minify: true,
+  	rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  },
+  plugins: [
+    dts({ insertTypesEntry: true }),
+  ],
+})
